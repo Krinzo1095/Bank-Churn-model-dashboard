f09a85aae149aca798cc228bcec4cbf459e45eeb
Krish Solanki <krishsolanki303@gmail.com>
2025-10-27 21:49:57 +0530

    Update back.py

--- back.py
+++ back.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from sklearn.experimental import enable_iterative_imputer  # needed before import
+from sklearn.experimental import enable_iterative_imputer 
 from sklearn.impute import IterativeImputer
 from sklearn.compose import ColumnTransformer
 
